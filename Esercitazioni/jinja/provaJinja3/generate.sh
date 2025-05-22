5752440e5112d83492173ebdc2e188fe5426348a
Liviu Minzatu <liviu.minzatu@sourcesense.com>
2025-05-22 12:53:41 +0200

    aggiunto lavoro

---
+++ Esercitazioni/jinja/provaJinja3/generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! command -v jinja2 &> /dev/null
+then
+    echo "jinja2-cli non trovato. Installa con: pip3 install jinja2-cli[yaml]"
+    exit 1
+fi
+
+jinja2 Dockerfile.j2 variables.yaml --format=yaml > Dockerfile
+echo "✅ Dockerfile generato!"
